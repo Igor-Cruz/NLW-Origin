d10d2761e4830d4a72db3e1989fb1877ffd3cbbd
Igor-Cruz <igorcruz.it@outlook.com>
2021-06-24 18:23:52 -0300

    After class 4 was completed, the footer was produced (html structure, social networks and styling), the back up button was created (html structure, style and js logic) and the concept and beginning of the production of media queries was introduced.

--- script.js
+++ script.js
@@ -19,10 +19,10 @@ for (const link of links) {
 
 /*Mudar o header da página quando der o scroll*/
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     //scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -30,7 +30,7 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
     //menor que a altura do header
   }
-})
+}
 
 /*Testimonials carousel slider swiper*/
 const swiper = new Swiper('.swiper-container', {
@@ -51,11 +51,31 @@ const scrollReveal = ScrollReveal({
 })
 
 scrollReveal.reveal(
-  `#home .image, #home .text
-#about .image, #about .text,
-#services header, #services .card,
-#testimonials header, #testimonials .testimonials #contact .text, #contact .links
-`,
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
 
   { interval: 100 }
 )
+
+/*Botão voltar ao topo*/
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+// window.addEventListener('scroll', function () {})
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
